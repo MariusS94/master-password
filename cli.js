f019627d666284937023d80871a77672a4d08be5
MariusS94 <marius1spomer@gmail.com>
2020-08-24 23:07:09 +0200

    Outsourced readpassword and questions

--- cli.js
+++ cli.js
@@ -1,66 +1,24 @@
-/* const readlineSync = require("readline-sync");
-
-const password = readlineSync.question("Put in your password ", {
-  hideEchoBack: true,
-});
-console.log(password); */
-
-const inquirer = require(`inquirer`);
-const fs = require("fs");
-
-const questionsStart = [
-  {
-    type: "password",
-    name: "password",
-    message: "What's your master password?",
-  },
-
-  {
-    type: "list",
-    name: "action",
-    message: "What do you want to do?",
-    choices: ["Get a password", "Set a password"],
-  },
-];
-
-const questionsGet = [
-  {
-    type: "input",
-    name: "key",
-    message: "Which passsword do you need?",
-  },
-];
-
-const questionsSet = [
-  {
-    type: "input",
-    name: "key",
-    message: "Which Password do you like to set?",
-  },
-
-  {
-    type: "password",
-    name: "password",
-    message: "Enter your new password",
-  },
-];
+const { readPassword } = require("./lib/readpassword");
+const {
+  askStartQuestion,
+  askGetQuestions,
+  askSetQuestion,
+} = require("./lib/questions");
 
 async function main() {
-  const { password, action } = await inquirer.prompt(questionsStart);
-
+  const { password, action } = await askStartQuestion();
   if (password === "123") {
     console.log("Password is right");
     if (action === "Get a password") {
-      const { key } = await inquirer.prompt(questionsGet);
+      const { key } = await askGetQuestions();
       try {
-        const myPasswords = fs.readFileSync(`./password.json`, `utf8`);
-        let data = JSON.parse(myPasswords);
-        console.log(`Your ${key} password is ${data[key]}`);
+        const password = await readPassword(key);
+        console.log(`Your ${key} password is ${password}`);
       } catch (error) {
         console.error("Something went wrong");
       }
     } else if (action === "Set a password") {
-      const { key, password } = await inquirer.prompt(questionsSet);
+      const { key, password } = await askSetQuestion();
       console.log(`New Password: ${key} to ${password}`);
     }
   } else {
