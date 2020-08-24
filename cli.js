869ce44745969216fbae82550d3d57a82eafbf39
MariusS94 <marius1spomer@gmail.com>
2020-08-24 21:10:09 +0200

    Add set password

--- cli.js
+++ cli.js
@@ -8,12 +8,22 @@ console.log(password); */
 const inquirer = require(`inquirer`);
 const fs = require("fs");
 
-const questions = [
+const questionsStart = [
   {
     type: "password",
     name: "password",
     message: "What's your master password?",
   },
+
+  {
+    type: "list",
+    name: "action",
+    message: "What do you want to do?",
+    choices: ["Get a password", "Set a password"],
+  },
+];
+
+const questionsGet = [
   {
     type: "input",
     name: "key",
@@ -21,18 +31,41 @@ const questions = [
   },
 ];
 
-inquirer.prompt(questions).then((answers) => {
-  /* console.log(`Your password is ${answers.password}!`); */
-  /* console.log(`Would you like to know the password of ${answers.key}?`); */
+const qustionSet = [
+  {
+    type: "input",
+    name: "key",
+    message: "Which Password do you like to set?",
+  },
 
+  {
+    type: "password",
+    name: "password",
+    message: "Enter your new password",
+  },
+];
+
+inquirer.prompt(questionsStart).then((answers) => {
   if (answers.password === "123") {
     console.log("Password is right");
-    try {
-      const myPasswords = fs.readFileSync(`./password.json`, `utf8`);
-      let data = JSON.parse(myPasswords);
-      console.log(`Your ${answers.key} password is ${data[answers.key]}`);
-    } catch (error) {
-      console.error("Something went wrong");
+    if (answers.action === "Get a password") {
+      inquirer.prompt(questionsGet).then((answers) => {
+        try {
+          const myPasswords = fs.readFileSync(`./password.json`, `utf8`);
+          let data = JSON.parse(myPasswords);
+          console.log(`Your ${answers.key} password is ${data[answers.key]}`);
+        } catch (error) {
+          console.error("Something went wrong");
+        }
+      });
+    } else if (answers.action === "Set a password") {
+      inquirer
+        .prompt(qustionSet)
+        .then((answersSet) =>
+          console.log(
+            `New Password: ${answersSet.key} to ${answersSet.password}`
+          )
+        );
     }
   } else {
     console.log("Master password is wrong");
