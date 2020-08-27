039a9a9e0f61de52aa3171193b41ce2b1e18fd4b
MariusS94 <marius1spomer@gmail.com>
2020-08-27 17:56:13 +0200

    Add get and post request function

--- server.js
+++ server.js
@@ -1,22 +1,43 @@
+require("dotenv").config();
 const express = require("express");
+
+const { MongoClient } = require("mongodb");
+const { readPassword, writePassword } = require("./lib/passwords");
+const bodyParser = require("body-parser");
 const app = express();
+const { encrypt, decrypt } = require("./lib/crypto");
 
-app.listen(3000, function () {
-  console.log("listening on 3000");
+const port = 3000;
+
+const client = new MongoClient(process.env.MONGO_URL, {
+  useUnifiedTopology: true,
 });
 
-app.get("/", (req, res) => {
-  res.send("hey guys");
-});
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+async function main() {
+  await client.connect();
+  const database = client.db(process.env.MONGO_DB_NAME);
+  const masterPassword = process.env.MASTER_PASSWORD;
 
-app.get("/api/passwords/wifi", (req, res) => {
-  res.send("123");
-});
+  app.get("/password/:name", async (request, response) => {
+    const { name } = request.params;
+    const password = await readPassword(name, database);
+    const decryptedPassword = decrypt(password, masterPassword);
 
-app.get("/api/passwords/wifi", (req, res) => {
-  res.send("123");
-});
+    response.send(decryptedPassword);
+  });
+
+  app.post("/password", async (request, response) => {
+    const { name, value } = request.body;
+    const encryptedPassword = encrypt(value, masterPassword);
+    await writePassword(name, encryptedPassword, database);
+    response.status(201).send("Password created");
+  });
+
+  app.listen(port, function () {
+    console.log(`listening on http://localhost:${port}`);
+  });
+}
+
+main();
