10e6947eca0f535ad036b47dac7c315cc4144728
MariusS94 <marius1spomer@gmail.com>
2020-08-27 13:46:42 +0200

    Install nodemon

--- server.js
+++ server.js
@@ -5,10 +5,18 @@ app.listen(3000, function () {
   console.log("listening on 3000");
 });
 
-/* app.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("hey guys");
-}); */
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/passwords/wifi", (req, res) => {
+  res.send("123");
+});
+
+app.get("/api/passwords/wifi", (req, res) => {
+  res.send("123");
 });
